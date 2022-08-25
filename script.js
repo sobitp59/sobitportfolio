27aaddbeb2dde2bcf1905fbfee7fa5bb3862da48
Sobit Prasad <sobitp59@gmail.com>
2022-08-25 20:51:40 +0530

    portfolio in making

---
+++ script.js
@@ -0,0 +1,14 @@
+let primaryNav = document.querySelector('.primary-navbar')
+let navToggleBtn = document.querySelector('.nav-toggle')
+
+navToggleBtn.addEventListener('click', ()=>{
+    let visibility = primaryNav.getAttribute('data-visible')
+    
+    if(visibility === 'false'){
+        primaryNav.setAttribute('data-visible', true)
+        navToggleBtn.setAttribute('aria-expanded', true)
+    }else{
+        primaryNav.setAttribute('data-visible', false)
+        navToggleBtn.setAttribute('aria-expanded', false)
+    }
+})
