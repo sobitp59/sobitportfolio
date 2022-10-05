c8c70f349eda5d1d1f32005465d134a462ee0291
Sobit Prasad <sobitp59@gmail.com>
2022-10-05 20:38:30 +0530

    type

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let primaryNav = document.querySelector('.primary-navbar')
 let navToggleBtn = document.querySelector('.nav-toggle')
-let aboutPersona = document.querySelector('.about__persona')
 
 
 // navbar for mobile 
@@ -17,41 +16,4 @@ navToggleBtn.addEventListener('click', ()=>{
 })
 
 
-// typewriter effect
-let phrases = ['Frontend Developer...', 'Technical Writer...', 'Constant Learner']
-let phraseIndex = 0;
-let letterIndex = 0;
 
-function writeText(phrase){
-
-    if(letterIndex == phrase.length){
-        eraseText();
-    }else if(letterIndex < phrase.length){
-        aboutPersona.textContent += phrase[letterIndex]
-        letterIndex+=1;
-        
-        setTimeout(()=>{
-            writeText(phrase);
-        }, 150)
-    }
-}
-
-
-function eraseText(){
-    let updatedPhrase = "";
-    if(letterIndex == -1){
-        phraseIndex = (phraseIndex + 1) % phrases.length;
-        letterIndex = 0;
-        writeText(phrases[phraseIndex]);
-    }else if(letterIndex > -1){
-        for(let i = 0; i < letterIndex; i++){
-            updatedPhrase += phrases[phraseIndex].charAt(i)
-        }
-
-        aboutPersona.textContent = updatedPhrase;
-        letterIndex-=1;
-        setTimeout(eraseText, 100);
-    }
-}
-
-writeText(phrases[phraseIndex])
